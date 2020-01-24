3abba05229009738e2a697b17999f8fec99f0caa
Quinn Slack <qslack@qslack.com>
2020-01-24 07:38:29 -0800

    remove compat check for 9+-month-old Sourcegraph version

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,6 @@ import { registerCompanyCompletionProvider } from './completion'
 import { registerLinkPreviewProvider } from './linkPreview'
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
-    if (
-        !sourcegraph.content ||
-        !sourcegraph.content.registerLinkPreviewProvider ||
-        !sourcegraph.languages.registerCompletionItemProvider
-    ) {
-        ctx.subscriptions.add(
-            sourcegraph.app.activeWindowChanges.subscribe(activeWindow => {
-                if (activeWindow) {
-                    activeWindow.showNotification(
-                        'To use the HubSpot extension, you must upgrade to Sourcegraph 3.3 (or the latest version of the browser extension).',
-                        sourcegraph.NotificationType.Error
-                    )
-                }
-            })
-        )
-        return
-    }
     ctx.subscriptions.add(registerLinkPreviewProvider())
     ctx.subscriptions.add(registerCompanyCompletionProvider())
 }
